89fd94a866be766e5de5c2152c3e94d3568563fa
Khushneet Inder Singh <khushneet.inder.singh@live.com>
2021-11-27 19:48:20 +0530

    Initial check-in with the base project.

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Khushneet Inder Singh on 27/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
