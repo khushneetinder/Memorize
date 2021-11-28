c487b49a8814a933904fe8da2a80f1badc8c5ed9
Khushneet Inder Singh <khushneet.inder.singh@live.com>
2021-11-28 12:15:11 +0530

    - Implemented Card view and its FaceUp functionality. - Added array of emojis and iterated over it to create a cardview by making them identifiable. - Added Add and Remove functionality using a button with image on it. - Added Dark theme Previewer.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+	let emojis = ["🚌", "🚜", "🛵", "🛴"]
+	@State var emojiCount = 4
 	var body: some View {
-		Text("Hello, world!")
-			.padding()
+		VStack {
+			HStack {
+				ForEach(emojis[0..<emojiCount], id:\.self) { emoji in
+					CardView(content: emoji)
+				}
+			}
+			Spacer()
+			HStack {
+				add
+				Spacer()
+				remove
+			}
+			.font(.largeTitle)
+			.padding(.horizontal)
+		}
+		.padding(.horizontal)
+		.foregroundColor(.red)
+	}
+	
+	var add : some View {
+		Button(action : {
+			if (emojiCount < emojis.count) {
+				emojiCount += 1
+			}
+		}, label : {
+			Image(systemName:"plus.circle")
+		}
+		)
+	}
+	
+	var remove : some View {
+		Button(action : {
+			if (emojiCount > 1) {
+				emojiCount -= 1
+			}
+		}, label : {
+			Image(systemName:"minus.circle")
+		}
+		)
+	}
+}
+
+struct CardView : View {
+	var content : String
+	@State var isFaceUp : Bool = true
+	
+	var body: some View {
+		ZStack {
+			let shape = RoundedRectangle(cornerRadius: 20)
+			if isFaceUp {
+				shape.fill().foregroundColor(.white)
+				shape.stroke(lineWidth: 3)
+				Text(content).font(.largeTitle)
+			}
+			else {
+				shape.fill()
+			}
+		}
+		.onTapGesture {
+			isFaceUp = !isFaceUp
+		}
 	}
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		ContentView()
+			.preferredColorScheme(.light)
+		ContentView()
+			.preferredColorScheme(.dark)
 	}
 }
